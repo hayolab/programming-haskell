16fe00720af6c944e6aa883de86108aa9adc1755
nabeen <nabeen@users.noreply.github.com>
2023-06-06 16:56:11 +0000

    feat: safetail

---
+++ src/chapter4/3.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+
+safetail' :: [a] -> [a]
+safetail' xs = if null xs then [] else tail xs
+
+safetail'' :: [a] -> [a]
+safetail'' xs
+  | null xs = []
+  | otherwise = tail xs
+
+safetail''' :: [a] -> [a]
+safetail''' [] = []
+safetail''' (_ : xs) = xs
